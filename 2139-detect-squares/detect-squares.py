1d0166010c82939004e3925b18877c9e48965ee9
Anirudh Herady <86644133+avh17@users.noreply.github.com>
2026-04-28 12:56:55 -0700

    Time: 276 ms (9.81%) | Memory: 21.3 MB (18.14%) - LeetSync

---
+++ 2139-detect-squares/detect-squares.py
@@ -0,0 +1,25 @@
+class DetectSquares:
+
+    def __init__(self):
+        self.ptsCount = defaultdict(int)
+        self.points = []
+
+    def add(self, point: List[int]) -> None:
+        self.ptsCount[tuple(point)]+=1
+        self.points.append(point)
+
+    def count(self, point: List[int]) -> int:
+        res = 0
+        px, py = point
+        for x, y in self.points:
+            if (abs(px-x)!=abs(py-y)) or x==px or y==py:
+                continue
+            res += (self.ptsCount[(x, py)])*(self.ptsCount[(px, y)])
+
+        return res
+
+
+# Your DetectSquares object will be instantiated and called as such:
+# obj = DetectSquares()
+# obj.add(point)
+# param_2 = obj.count(point)
